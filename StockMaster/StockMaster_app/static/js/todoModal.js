69ca5fc6da9fcddc22e262be28d9dab3e2b09f57
MeganPegueros <mpegueros@ucol.mx>
2023-11-08 09:54:37 -0600

    Campo select grupo, eliminado

--- StockMaster/StockMaster_app/static/js/todoModal.js
+++ StockMaster/StockMaster_app/static/js/todoModal.js
@@ -1,3 +1,10 @@
-document.querySelectorAll(".campos[type='text'], .registrar[type='text']").forEach(function(input) {
+/* document.querySelectorAll(".campos[type='text'], .registrar[type='text']").forEach(function(input) {
   input.value = input.value.toUpperCase();
-});
+}); */
+
+function convertToUppercase() {
+  var inputElements = document.querySelectorAll(".campos, .registrar");
+  for (var i = 0; i < inputElements.length; i++) {
+    inputElements[i].value = inputElements[i].value.toUpperCase();
+  }
+}

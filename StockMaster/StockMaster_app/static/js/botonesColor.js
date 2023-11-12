d2ecb6994599feb45fbdb9ee932b7a9b0a6c27d8
MeganPegueros <mpegueros@ucol.mx>
2023-11-12 15:18:50 -0600

    Arreglar Select

--- StockMaster/StockMaster_app/static/js/botonesColor.js
+++ StockMaster/StockMaster_app/static/js/botonesColor.js
@@ -1,42 +1,4 @@
-/* const configurableButtons = document.querySelectorAll(".configurable-button");
-const productCards = document.querySelectorAll(".prod");
-const scrollContainer = document.querySelector(".scroll-container");
-const scrollLeftButton = document.querySelector(".scroll-button.left-button");
-const scrollRightButton = document.querySelector(".scroll-button.right-button");
-
-let scrollPosition = 0;
-
-function scrollButtons(direction) {
-    const buttonContainerWidth = scrollContainer.clientWidth;
-    const buttonWidth = configurableButtons[0].offsetWidth;
-    const maxScroll = scrollContainer.scrollWidth - buttonContainerWidth;
-
-    scrollPosition += direction * buttonWidth;
-    scrollPosition = Math.max(0, Math.min(scrollPosition, maxScroll));
-
-    scrollContainer.style.transform = `translateX(-${scrollPosition}px)`;
-}
-
-scrollLeftButton.addEventListener("click", () => {
-    scrollButtons(-1);
-});
-
-scrollRightButton.addEventListener("click", () => {
-    scrollButtons(1);
-});
-
-configurableButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-        productCards.forEach((card) => {
-            card.style.display = "none";
-        });
-        productCards[index].style.display = "block";
-    });
-}); */
-
-
-
-/* const configurableButtons = document.querySelectorAll(".configurable-button");
+const configurableButtons = document.querySelectorAll(".configurable-button");
 const productCards = document.querySelectorAll(".prod");
 const scrollContainer = document.querySelector(".scroll-container");
 const scrollLeftButton = document.querySelector(".scroll-button.left-button");
@@ -45,7 +7,8 @@ const scrollRightButton = document.querySelector(".scroll-button.right-button");
 let scrollPosition = 0;
 let selectedButton = null; // Mantener un registro del botón seleccionado
 
-function scrollButtons(direction) {
+function scrollButtons(event, direction) {
+
     const buttonContainerWidth = scrollContainer.clientWidth;
     const buttonWidth = configurableButtons[0].offsetWidth;
     const maxScroll = scrollContainer.scrollWidth - buttonContainerWidth;
@@ -53,95 +16,29 @@ function scrollButtons(direction) {
     scrollPosition += direction * buttonWidth;
     scrollPosition = Math.max(0, Math.min(scrollPosition, maxScroll));
 
-    scrollContainer.style.transform = `translateX(-${scrollPosition}px}`;
-}
-
-function activateButton(button) {
-    if (selectedButton) {
-        selectedButton.classList.remove("selected");
-    }
-    button.classList.add("selected");
-    selectedButton = button;
-}
-
-scrollLeftButton.addEventListener("click", () => {
-    scrollButtons(-1);
-});
-
-scrollRightButton.addEventListener("click", () => {
-    scrollButtons(1);
-});
-
-configurableButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-        productCards.forEach((card) => {
-            card.style.display = "none";
-        });
-        productCards[index].style.display = "block";
-        activateButton(button);
-    });
-}); */
-
-
-const configurableButtons = document.querySelectorAll(".configurable-button");
-const productCards = document.querySelectorAll(".prod");
-const scrollContainer = document.querySelector(".scroll-container");
-const scrollLeftButton = document.querySelector(".scroll-button.left-button");
-const scrollRightButton = document.querySelector(".scroll-button.right-button");
-let scrollPosition = 0;
-let selectedButton = null; // Mantén un registro del botón seleccionado
-
-function scrollButtons(direction) {
-    const buttonContainerWidth = scrollContainer.clientWidth;
-    const buttonWidth = configurableButtons[0].offsetWidth;
-    const maxScroll = scrollContainer.scrollWidth - buttonContainerWidth;
-    scrollPosition += direction * buttonWidth;
-    scrollPosition = Math.max(0, Math.min(scrollPosition, maxScroll));
     scrollContainer.style.transform = `translateX(-${scrollPosition}px)`;
 }
 
 function activateButton(button) {
-    if (selectedButton) {
-        selectedButton.classList.remove("selected");
-    }
-    button.classList.add("selected");
-    selectedButton = button;
+    console.log("Botón activado:", button);
+    // Desactivar todos los botones
+    configurableButtons.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+    // Activar el botón seleccionado
+    button.classList.add("active");
 }
 
-configurableButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-        // Productos relacionados con la categoría seleccionada
-        const categoriaId = button.value;
-        // Realiza una solicitud a tu servidor para obtener los productos de la categoría
-        fetch(`/api/productos?categoriaId=${categoriaId}`)
-            .then((response) => response.json())
-            .then((data) => {
-                // Oculta todos los productos
-                productCards.forEach((card) => {
-                    card.style.display = "none";
-                });
-                // Muestra los productos obtenidos
-                data.forEach((producto) => {
-                    const index = productos.findIndex((p) => p === producto);
-                    productCards[index].style.display = "block";
-                });
-            })
-            .catch((error) => {
-                console.error("Error al obtener los productos", error);
-            });
-
-        scrollButtons(index); // Desplaza el contenedor al botón seleccionado
+configurableButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
         activateButton(button); // Activa el botón seleccionado
+        scrollButtons(event, 0); // Llama a scrollButtons con dirección 0
+        // Tu código adicional aquí para responder al clic del botón
     });
 });
 
-scrollLeftButton.addEventListener("click", () => {
-    scrollButtons(-1);
-});
 
-scrollRightButton.addEventListener("click", () => {
-    scrollButtons(1);
-});
+
 
 
 

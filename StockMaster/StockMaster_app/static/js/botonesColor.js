7f11930f1f51fe7199fb4b49a136e4e53930f927
MeganPegueros <mpegueros@ucol.mx>
2023-11-12 18:02:07 -0600

    Ya recorre el menu de botones

--- StockMaster/StockMaster_app/static/js/botonesColor.js
+++ StockMaster/StockMaster_app/static/js/botonesColor.js
@@ -1,41 +1,34 @@
-const configurableButtons = document.querySelectorAll(".configurable-button");
-const productCards = document.querySelectorAll(".prod");
-const scrollContainer = document.querySelector(".scroll-container");
-const scrollLeftButton = document.querySelector(".scroll-button.left-button");
-const scrollRightButton = document.querySelector(".scroll-button.right-button");
+const scrollableButtons = document.querySelector('.scrollable-buttons');
+const scrollRightButton = document.querySelector('.scroll-button.right-button');
+const scrollLeftButton = document.querySelector('.scroll-button.left-button');
+const buttonWidth = document.querySelector('.configurable-button').offsetWidth;
 
-let scrollPosition = 0;
-let selectedButton = null; // Mantener un registro del botón seleccionado
-
-function scrollButtons(event, direction) {
-
-    const buttonContainerWidth = scrollContainer.clientWidth;
-    const buttonWidth = configurableButtons[0].offsetWidth;
-    const maxScroll = scrollContainer.scrollWidth - buttonContainerWidth;
-
-    scrollPosition += direction * buttonWidth;
-    scrollPosition = Math.max(0, Math.min(scrollPosition, maxScroll));
-
-    scrollContainer.style.transform = `translateX(-${scrollPosition}px)`;
-}
-
-function activateButton(button) {
-    console.log("Botón activado:", button);
-    // Desactivar todos los botones
-    configurableButtons.forEach((btn) => {
-        btn.classList.remove("active");
-    });
-    // Activar el botón seleccionado
-    button.classList.add("active");
-}
-
-configurableButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-        activateButton(button); // Activa el botón seleccionado
-        scrollButtons(event, 0); // Llama a scrollButtons con dirección 0
-        // Tu código adicional aquí para responder al clic del botón
-    });
+scrollLeftButton.addEventListener('click', () => {
+  scrollButtons(-1);
 });
+
+scrollRightButton.addEventListener('click', () => {
+  scrollButtons(1);
+});
+
+function scrollButtons(direction) {
+  const currentScrollPosition = scrollableButtons.scrollLeft;
+  const targetScrollPosition = currentScrollPosition + direction * buttonWidth;
+
+  scrollableButtons.scrollTo({
+    left: targetScrollPosition,
+    behavior: 'smooth' // Hace que el desplazamiento sea suave
+  });
+}
+
+
+
+
+
+
+
+
+
 
 
 

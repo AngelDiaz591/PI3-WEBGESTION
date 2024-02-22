4e657baa7d903d0cb4821af4f5735c052440bbbc
IsmaelMontiel710 <112568943+IsmaelMontiel710@users.noreply.github.com>
2024-02-22 13:08:21 -0600

    bd añadida

--- StockMaster/StockMaster/settings.py
+++ StockMaster/StockMaster/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kq*5v=aq7s&mh0079d)$nkh1qa%#hy$z4-#0-z^9$p&fo*jz5@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
